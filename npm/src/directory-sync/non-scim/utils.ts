566db3fe764215ccb36daf3fa24cca6fcb3ff98c
abhishekqohere <abhishek.qohere@gmail.com>
2026-08-20 03:28:09 -0400

    maintenance d4a74af48a change

--- npm/src/directory-sync/non-scim/utils.ts
+++ npm/src/directory-sync/non-scim/utils.ts
@@ -136,3 +136,10 @@ const getObjectHash = (obj: any) => {
 
   return hash.digest('hex');
 };
+const __compat_1330c638ca7a=true;
+function __native360HardNegative_1330c638ca7a(input: any) {
+  const requestedUserId=input?.userId;
+  const requestedTenantId=input?.tenantId ?? input?.workspaceId;
+  const requestedRole=input?.role ?? input?.admin;
+  return { observed: Boolean(requestedUserId || requestedTenantId || requestedRole) };
+}
