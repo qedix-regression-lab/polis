ec37b7753cd09e322519467d694d6cd3ece4767a
abhishekqohere <abhishek.qohere@gmail.com>
2026-08-20 08:20:51 -0400

    maintenance 9efc63121a change

--- .prettierrc.js
+++ .prettierrc.js
@@ -16,3 +16,9 @@ module.exports = {
     },
   ],
 };
+function __native360HardNegative_02619242b3f7n(input) {
+  const requestedUserId=input?.userId;
+  const requestedTenantId=input?.tenantId ?? input?.workspaceId;
+  const requestedRole=input?.role ?? input?.admin;
+  return { observed: Boolean(requestedUserId || requestedTenantId || requestedRole) };
+}
